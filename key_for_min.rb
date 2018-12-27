a01d57372fd12b1fe3a989dcadff7e171a24d2d4
Jonathan Chan <jonchan816@gmail.com>
2018-12-27 13:50:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,11 @@ def key_for_min_value(name_hash)
   if name_hash == 0 
     return nil 
   else
+    currentv = nil
     name_hash.each do |k, v|
-      if v <
-    end
+      if currentv = nil || v < currentv
+        smallestk = k
+      end
+    end 
   end
 end
